3f1179e97abf48dd9da23bbf7cea7f108d4dac05
Luc-cpl <lucas.carvalhopl@outlook.com>
2024-10-07 18:22:54 -0300

    Refactor property types in SkuSpecification.php

--- src/Entities/Catalog/SkuSpecification.php
+++ src/Entities/Catalog/SkuSpecification.php
@@ -7,7 +7,7 @@ use Orkestra\Entities\AbstractEntity;
 /**
  * @property-read int $skuId
  * @property-read int $fieldId
- * @property-read int $fieldValueId
+ * @property-read int|null $fieldValueId
  * @property-read int|null $id
  * @property-read string|null $text
  */
@@ -16,7 +16,7 @@ class SkuSpecification extends AbstractEntity
 	public function __construct(
 		protected int $skuId,
 		protected int $fieldId,
-		protected int $fieldValueId,
+		protected ?int $fieldValueId,
 		protected ?int $id = null,
 		protected ?string $text = null,
 	) {
